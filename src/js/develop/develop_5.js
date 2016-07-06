ee37bd1ee6646a12d3bd5b3b5f8cae789a32996f
nickolay Gotsliyk <trolizmaslom@gmail.com>
2016-07-06 17:56:08 +0300

    header

--- src/js/develop/develop_5.js
+++ src/js/develop/develop_5.js
@@ -1,7 +1,29 @@
+function butter() {
+    $('.butter').click(function (e) {
+        e.preventDefault();
+        $(this).toggleClass('active');
+        if($(this).hasClass('active')){
+            $('.menu').stop().slideDown();
+        } else{
+            $('.menu').stop().slideUp();
+        }
+    });
+    $(document).on('click touchstart',function (event){
+        if($('.butter').css('display') == 'block') {
+            var div = $('.menu');
+            if (!div.is(event.target) && div.has(event.target).length === 0 && !$('.butter').is(event.target) && $('.butter').has(event.target).length === 0)
+            {
+                $('.menu').slideUp();
+                $('.butter').removeClass('active');
+            }
+        }
+    });
+}
 
 
 
 $(document).ready(function(){
+    butter();
 
 });
 
@@ -10,5 +32,5 @@ $(window).load(function(){
 });
 
 $(window).resize(function(){
-
+    if(!$('.butter').css('display') == 'block'){$('.menu').removeAttr('style');}
 });
