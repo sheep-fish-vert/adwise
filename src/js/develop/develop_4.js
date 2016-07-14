bb989e887755c6abf8e23b467e2e9ad38bcd01ce
radiks <radion91@gmail.com>
2016-07-14 16:05:10 +0300

    creatives.html

--- src/js/develop/develop_4.js
+++ src/js/develop/develop_4.js
@@ -11,25 +11,70 @@ function massonryImage(){  // in news page
 function creativesSlider(){
     if( $('.creatives').length ){
 
+        var slidesToShow = 4;
+
+
         $('.creatives-slider').on('init', function(event, slick, direction){
             $('.creatives-wrap').addClass('active');
+            var items = $(this).find('.item');
+            if ( items.length <= slidesToShow ){
+                $(this).find('.slick-list').addClass('remove-right'); //remove shadow on init
+            }else if(items.length > slidesToShow){
+                $(this).addClass('left-hide-button');//remove button
+            }
         });
 
         $('.creatives-slider').each(function(index, el) {
             var elem = el;
+            var that = $(this);
+
+            $(this).on('init', function(event, slick, direction){
+                $(this).find('.slick-list').addClass('remove-left'); //?
+            });
+
+            $(this).on('afterChange', function(event, slick, direction){
+                checkItem(that);
+            });
 
             $(this).slick({
-                slidesToShow: 4,
+                slidesToShow: slidesToShow,
                 slidesToScroll: 1,
                 dots: false,
                 infinite: false,
                 prevArrow:'<button type="button" class="slick-prev"></button>',
-                nextArrow:'<button type="button" class="slick-next">Next</button>'
+                nextArrow:'<button type="button" class="slick-next"></button>'
 
             });
         });
 
+        function checkItem(that){
+            var items = that.find('.item');
+            var first = items.first();
 
+            if( first.is('.slick-current.slick-active') ){ //левый шадоу
+                that.find('.slick-list').addClass('remove-left');
+                that.find('.slick-list').removeClass('remove-right');
+
+                // button hide
+                that.addClass('left-hide-button');
+            }else{
+                that.find('.slick-list').removeClass('remove-left');
+                that.removeClass('left-hide-button');
+            }
+
+            var magicCount =(items.length - (items.length - slidesToShow)) - 2; // супер
+
+            if( items.eq( magicCount ).is('.slick-current.slick-active') || items.eq( magicCount ).nextAll().is('.slick-current.slick-active')) { //правый швдоу
+                that.find('.slick-list').removeClass('remove-left');
+                that.find('.slick-list').addClass('remove-right');
+
+                // button hide
+                that.addClass('right-hide-button');
+            }else{
+                that.find('.slick-list').removeClass('remove-right');
+                that.removeClass('right-hide-button');
+            }
+        }
 
 
 
