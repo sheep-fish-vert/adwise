711e8ee5434adb18a2907a985bc8396621e98fbc
radiks <radion91@gmail.com>
2016-07-14 12:25:27 +0300

    creative 50%

--- src/js/develop/develop_4.js
+++ src/js/develop/develop_4.js
@@ -8,6 +8,41 @@ function massonryImage(){  // in news page
     });
 }
 
+function creativesSlider(){
+    if( $('.creatives').length ){
+
+        $('.creatives-slider').on('init', function(event, slick, direction){
+            $('.creatives-wrap').addClass('active');
+        });
+
+        $('.creatives-slider').each(function(index, el) {
+            var elem = el;
+
+            $(this).slick({
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                dots: false,
+                infinite: false,
+                prevArrow:'<button type="button" class="slick-prev"></button>',
+                nextArrow:'<button type="button" class="slick-next">Next</button>'
+
+            });
+        });
+
+
+
+
+
+        // tabs on slider
+        $('.tab-item').not(':first').removeClass('show');
+        $('.main-wrap.tabs .column-list-main li').click(function(event){
+            event.preventDefault();
+            $('.main-wrap.tabs .column-list-main li').removeClass('active').eq($(this).index()).addClass('active');
+            $('.tab-item').removeClass('show').eq($(this).index()).addClass('show');
+        }).eq(0).addClass('active');
+    }
+}
+
 
 $(document).ready(function(){
 
@@ -15,6 +50,7 @@ $(document).ready(function(){
 
 $(window).load(function(){
     massonryImage();
+    creativesSlider();
 });
 
 $(window).resize(function(){
