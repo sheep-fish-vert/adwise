ec4a16659b2e51492628a6ff70747ddbca4b069a
radiks <radion91@gmail.com>
2016-07-20 17:58:29 +0300

    parallax checkpoint

--- src/js/develop/develop_1.js
+++ src/js/develop/develop_1.js
@@ -1,5 +1,64 @@
+/* parallax scroll */
+
+    function parallaxScroll(){
+
+        // page params
+
+        var windowHeight = 0;
+        var blurParallaxPadding = 0;
+        var imageTopDiferent = 0;
+        var imageBottomDiferent = 0;
+
+        // page params write
+
+        function pageParamsWrite(){
+
+            windowHeight = $(window).height();
+
+            blurParallaxPadding = parseInt($('.main').css('padding-top')) + $('.header').height();
+
+            imageTopDiferent = $('.parallax-images .parallax-image-top img').height() - windowHeight;
+            if($('.parallax-image-bottom').length){
+                imageBottomDiferent = $('.parallax-images .parallax-image-bottom img').height() - windowHeight;
+            }
+
+        }
+
+        pageParamsWrite();
+
+
+        // parallax scrolling
+
+        $(window).scroll(function(){
+
+            var scrolled = $(window).scrollTop();
+            var imgScroller = parseInt(scrolled * 1);
+            console.log(imgScroller, imageTopDiferent);
+            if(imgScroller < imageTopDiferent){
+
+                $('.parallax-images').css({'top':'-'+imgScroller+'px'});
+
+            }else{
+                $('parallax-images').css({'top':'-'+imageTopDiferent+'px'});
+            }
+
+            if(scrolled < blurParallaxPadding){
+                $('.parallax-blur').css({'top':'-'+scrolled+'px'});
+            }else{
+               $('.parallax-blur').css({'top':'-'+blurParallaxPadding+'px'});
+            }
+
+
+        });
+
+    }
+
+/* /parallax scroll */
+
 
 $(document).ready(function(){
+
+    parallaxScroll();
 
 });
 
