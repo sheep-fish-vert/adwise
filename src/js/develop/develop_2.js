68ef768cb8e758027560439bc7088e8abc0ff739
Artem <artem.garmatyk@gmail.com>
2016-07-26 16:03:17 +0300

    fix fade

--- src/js/develop/develop_2.js
+++ src/js/develop/develop_2.js
@@ -7,8 +7,8 @@ $(document).ready(function(){
         var defr =  $(this).closest('.tabber').find('.tab-content>.con') ;
         var curr = $(this).index();
         $(this).closest('.tabber').find('.tab-row>.tab').removeClass('active').eq(curr).addClass('active');
-        $(this).closest('.tabber').find('.tab-content>.active').slideUp(300, function(){
-            defr.removeClass('active').eq(curr).slideDown(300, function(){
+        $(this).closest('.tabber').find('.tab-content>.active').fadeOut(100, function(){
+            defr.removeClass('active').eq(curr).fadeIn(100, function(){
                 defr.removeClass('active').eq(curr).addClass('active');
             });
         })
