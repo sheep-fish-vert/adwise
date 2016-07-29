122facfefbf4283702d10ba61473e4138ec5e1e3
Nazar <kryvyynazariy@gmail.com>
2016-07-29 11:46:51 +0300

    font-changed

--- src/js/develop/develop_6.js
+++ src/js/develop/develop_6.js
@@ -1,8 +1,17 @@
-
+function changeFont(){
+	$('.item-big-number').each(function(){
+		var numOfLetters = $(this).html().length;
+		console.log(numOfLetters);
+		if(numOfLetters > 1){
+			$(this).css('font-family', 'CoreSansCn');
+		}
+	})
+	
+}	
 
 
 $(document).ready(function(){
-
+	changeFont()
 });
 
 $(window).load(function(){
