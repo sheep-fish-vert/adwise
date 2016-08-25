7e8480fda975319cc753bc60a842a9579adb2735
nickolay Gotsliyk <trolizmaslom@gmail.com>
2016-08-25 17:55:22 +0300

    qw

--- src/js/develop/develop_5.js
+++ src/js/develop/develop_5.js
@@ -280,15 +280,200 @@ function oneSizeForMainNews(){
 
 
 //video creatives---------------------------------------------------------------------------------
+function prerollFancyOpen() {
+    $(document).on('click', '.item-preroll a', function (e) {
+        var src = $(this).data('src');
+        var poster = $(this).data('poster');
+        var title = $(this).find('.title-item').text();
+        $('#video-popup .trollPlayer').trollPlayer({
+            src:src,
+            poster:poster
+        });
+        $.fancybox.open('#video-popup',{
+            openEffect  : 'fade',
+            closeEffect : 'fade',
+            width:'80%',
+            autoResize:true,
+            wrapCSS:'fancybox-standart',
+            'closeBtn' : false,
+            fitToView:true,
+            padding:'0'
+        });
+        e.preventDefault();
+        return false;
 
-    
+    })
+}
+    prerollFancyOpen();
 
+var wavesurfer
+function audioFancyOpen() {
+    $(document).on('click', '.item-audio a', function (e) {
+        if($(this).hasClass('initialized')){
+            $.fancybox.open('#audio-popup',{
+                'closeBtn' : false,
+                padding:0,
+                afterClose: function () {
+                    wavesurfer.stop();
+                }
+            });
+        }else {
+            $('.item-audio a').removeClass('initialized');
+            $('.play-bttn').removeClass('paused');
+            $('.play-bttn').unbind();
+            $('.stop-bttn').unbind();
+            var src = $(this).data('src');
+
+
+            $('#waveform').html('');
+
+            wavesurfer = WaveSurfer.create({
+                container: '#waveform',
+                waveColor: 'violet',
+                progressColor: 'purple'
+            });
+            wavesurfer.load(src);
+
+            $.fancybox.open('#audio-popup', {
+                'closeBtn': false,
+                padding:0,
+                afterClose: function () {
+                    wavesurfer.stop();
+                }
+            });
+            var hasHour;
+            wavesurfer.on('ready', function () {    //ивент определения  возможности проигрования
+                hasHours = (wavesurfer.getDuration() / 3600) >= 1.0;
+                $('#audio-popup .total-time').text(formatTime(wavesurfer.getDuration(), hasHours));
+                $('#audio-popup .curent-time').text(formatTime(0), hasHours);
+
+            });
+            wavesurfer.on('audioprocess', function () {
+                var current = wavesurfer.getCurrentTime();
+                var progress = (Math.floor(current) / Math.floor(wavesurfer.getDuration()));
+                $('#audio-popup .curent-time').text(formatTime(current, hasHours));
+                $('#audio-popup .curent-progress').css('width',(Math.floor(progress * $('.total-progress').width()) + "px"));
+
+            });
+            $('.total-progress').click(function(e) {// ивент клика по таймлайну (перемотка мышкой)
+                var x = (e.offsetX==undefined?e.layerX:e.offsetX)/$(this).width();
+                wavesurfer.seekTo(x);
+
+                $('#audio-popup .curent-progress').css('width',(e.offsetX==undefined?e.layerX:e.offsetX) + "px");
+            });
+            $(this).addClass('initialized');
+            $('.play-bttn').click(function () {
+                wavesurfer.playPause();
+                $(this).toggleClass('paused');
+            });
+            $('.mute-bttn').click(function () {
+                wavesurfer.toggleMute();
+                $(this).toggleClass('muted');
+            });
+        }
+        e.preventDefault();
+        return false;
+    });
+}
+
+
+
+function voiceoverFancyOpen() {
+    $(document).on('click', '.item-voiceover a', function (e) {
+
+            var srcVideo = $(this).data('srcvideo');
+            var poster = $(this).data('poster');
+            var title = $(this).find('.title-item').text();
+            $('.title-audio').text(title);
+            var start = parseInt($(this).data('start'));
+            var stop = parseInt($(this).data('stop'));
+            var wrap = $('.voiceover-container .trollPlayer').trollPlayer({
+                src: srcVideo,
+                poster: poster
+
+            });
+
+            var player = wrap.find('video')[0];
+
+
+            setTimeout(function () {
+
+                $.fancybox.open('#voiceover-popup',{
+                    'closeBtn' : false,
+                    padding:0,
+                    afterShow:function(){
+                        var vD = player.duration;
+                        var wT = $('.voiceover-container .trollPlayer').width();
+
+                        var aD = stop - start;
+                        var waveWidth = (wT * aD) / vD;
+                        var waveLeft = (start * wT) / vD;
+
+                        $('#waveformAudio').attr('style', 'width:' + waveWidth + 'px; left:' + waveLeft + 'px;');
+                        wrap.find('.time_voiceover').attr('style', 'width:' + waveWidth + 'px; left:' + waveLeft + 'px;');
+                        wrap.find('.time_voiceover-border').attr('style', 'width:' + waveWidth + 'px; left:' + waveLeft + 'px;');
+                        var wavesurferV = WaveSurfer.create({
+                            container: '#waveformVideo',
+                            waveColor: '#810000',
+                            progressColor: 'purple',
+                            backend: 'MediaElement'
+                        });
+                        wavesurferV.load(srcVideo);
+                        wavesurferV.setVolume(0);
+                        player.addEventListener("timeupdate", function () {
+                            var progress = Math.floor(player.currentTime) / Math.floor(player.duration);
+                            wavesurferV.seekTo(progress);
+                        }, false);
+                    },
+                    afterClose: function () {
+                        $('#waveformVideo').html('');
+                        wrap.trollPlayer('destroy');
+
+                        $('#waveformAudio').removeAttr('style');
+
+                    }
+                });
+
+            }, 300);
+
+
+
+        e.preventDefault();
+        return false;
+    });
+}
+// функция для форматирования времени
+function formatTime(time, hours) {
+    if (hours) {
+        var h = Math.floor(time / 3600);
+        time = time - h * 3600;
+
+        var m = Math.floor(time / 60);
+        var s = Math.floor(time % 60);
+
+        return h.lead0(2)  + ":" + m.lead0(2) + ":" + s.lead0(2);
+    } else {
+        var m = Math.floor(time / 60);
+        var s = Math.floor(time % 60);
+
+        return m.lead0(2) + ":" + s.lead0(2);
+    }
+}
+
+Number.prototype.lead0 = function(n) {
+    var nz = "" + this;
+    while (nz.length < n) {
+        nz = "0" + nz;
+    }
+    return nz;
+};
+// конец функции для форматирования времени
 //END video creatives---------------------------------------------------------------------------------
 
 $(document).ready(function(){
-    $('.trollPlayer').trollPlayer();
+    audioFancyOpen();
     butter();
-
+    voiceoverFancyOpen();
     menuSecondLevelHover();
 
     $.getJSON('data.json',function (data) {
--- src/js/plagins/troll.player.js
+++ src/js/plagins/troll.player.js
@@ -1,24 +1,5 @@
-// function trollPlayer() {
-//     var controls = {
-//         video: $("#myvideo"),
-//         playpause: $("#playpause")
-//     };
-//
-//     var video = controls.video[0];
-//
-//     controls.playpause.click(function(){
-//         if (video.paused) {
-//             video.play();
-//
-//         } else {
-//             video.pause();
-//         }
-//
-//         $(this).toggleClass("paused");
-//     });
-//
-// }
-var player;
+
+
 (function($) {
 
     var defaults = {
@@ -27,16 +8,18 @@ var player;
             stop:'stop_ctrl',
             timeLine:'timeline_ctrl',
             time:'time_ctrl',
-            sound:'sound_ctrl',
+
             fullScr:'fullscreen_ctrl',
-            smallScr:'smallscreen_ctrl'
+            sound:'sound_ctrl',
+            title:'title'
 
         },
-        ctrlsWrap:'vid-ctrls'
+        ctrlsWrap:'vid-ctrls',
+        bigPlay:'big-play'
 
     };
 
-    var state = 0 ;
+    var state = 0 ; // текущее состояние плеера; 0-стартовок; 1-проигрывается; 2-на паузе;
     var methods = {
         init:function(params) {
             var options = $.extend({}, defaults, params);
@@ -44,63 +27,181 @@ var player;
 
                 var objects = {
                         main:$(this),
-                        ctrlWrap : {},
                         ctrl:{
-                            play:{},
-                            stop:{},
-                            timeline:{},
-                            time:{},
-                            sound:{},
-                            fullScr:{},
-                            smallScr:{}
+                            hasHours: false
                         },
                         player:$(this).find('video')[0]
                     };
                 objects.ctrlWrap = methods.initUI(objects.main, options);
-
+                //сылки на обьекты
                objects.ctrl.play = objects.ctrlWrap.find('.'+options.ctrls.play);
                objects.ctrl.stop = objects.ctrlWrap.find('.'+options.ctrls.stop);
                objects.ctrl.timeline = objects.ctrlWrap.find('.'+options.ctrls.timeLine);
                objects.ctrl.time = objects.ctrlWrap.find('.'+options.ctrls.time);
                objects.ctrl.sound = objects.ctrlWrap.find('.'+options.ctrls.sound);
                objects.ctrl.fullScr = objects.ctrlWrap.find('.'+options.ctrls.fullScr);
-               objects.ctrl.smallScr = objects.ctrlWrap.find('.'+options.ctrls.smallScr);
-                player = objects.player;
-                console.log(player);
-                objects.ctrl.play.click(function () {
-                   if(state!=1){
-                       methods.playV(objects.player, options, objects);
-                   } else{
-                       methods.pauseV(objects.player, options, objects);
-                   }
+               objects.ctrl.buffered = objects.ctrlWrap.find('.time_buffer');
+               objects.ctrl.progress = objects.ctrlWrap.find('.time_current');
+               objects.ctrl.duration = objects.ctrlWrap.find('#duration');
+               objects.ctrl.currentTime = objects.ctrlWrap.find('#currenttime');
+               objects.bigPlay = objects.main.find('.'+options.bigPlay);
+                if(options.src != "undefined"){objects.player.src = options.src;}
+                if(options.poster != "undefined"){objects.player.poster = options.poster;}
+
+
+
+                var player = objects.player;
+
+
+
+
+                objects.player.addEventListener("canplay", function() {     //ивент определения  возможности проигрования
+                    objects.ctrl.hasHours = (objects.player.duration / 3600) >= 1.0;
+                    objects.ctrl.duration.text(formatTime(objects.player.duration, objects.ctrl.hasHours));
+                    objects.ctrl.currentTime.text(formatTime(0),objects.ctrl.hasHours);
+
                 });
+                objects.ctrl.play.click(function () {methods.togglePlay(options, objects) }); //ивент кноки плей
+                objects.bigPlay.click(function () {methods.togglePlay(options, objects) }); // ивент клика по видео
+                objects.ctrl.stop.click(function () {methods.stopV(options, objects) }); // ивент клика по кнопке стоп
+
+                objects.player.addEventListener("timeupdate", function() {  // ивент воспроизведения
+                    objects.ctrl.currentTime.text(formatTime(objects.player.currentTime, objects.ctrl.hasHours));
+                    var progress = Math.floor(objects.player.currentTime) / Math.floor(objects.player.duration);
+                    objects.ctrl.progress.css('width',(Math.floor(progress * objects.ctrl.timeline.width()) + "px"));
+                }, false);
+
+                
+                objects.player.addEventListener("ended", function() {// ивент окончания воспроизведения видео
+                    methods.stopV(options, objects);
+                });
+                objects.ctrl.sound.click(function() {// ивент клика по динамику
+                    methods.toggleMute(objects);
+                });
+                objects.ctrl.timeline.click(function(e) {// ивент клика по таймлайну (перемотка мышкой)
+                    var x = (e.offsetX==undefined?e.layerX:e.offsetX)/$(this).width();
+                    objects.player.currentTime = x * objects.player.duration;
+                });
+                objects.ctrl.fullScr.click(function () {
+                    objects.main.toggleClass('fullscreen');
+                    if(objects.main.hasClass('fullscreen')){
+                        methods.fullscreen($('.trollPlayer'));
+                        objects.player.controls = false;
+
+                    }else{
+                        methods.exitFullscreen();
+                    }
+                });
+                
+
+                // функция для форматирования времени
+                function formatTime(time, hours) {
+                    if (hours) {
+                        var h = Math.floor(time / 3600);
+                        time = time - h * 3600;
+
+                        var m = Math.floor(time / 60);
+                        var s = Math.floor(time % 60);
+
+                        return h.lead0(2)  + ":" + m.lead0(2) + ":" + s.lead0(2);
+                    } else {
+                        var m = Math.floor(time / 60);
+                        var s = Math.floor(time % 60);
+
+                        return m.lead0(2) + ":" + s.lead0(2);
+                    }
+                }
+
+                Number.prototype.lead0 = function(n) {
+                    var nz = "" + this;
+                    while (nz.length < n) {
+                        nz = "0" + nz;
+                    }
+                    return nz;
+                };
+                // конец функции для форматирования времени
+                if(options.callback){
+                    options.callback();
+                }
+                return  player;
             });
         },
-        initUI:function (main, options) {
+        initUI:function (main, options) { //метод инициализации UI
             mass = options.ctrls;
             var content = '<div class="'+options.ctrlsWrap+'">';
-            for(var key in mass){
-                content = content + '<div class="'+mass[key]+'" ></div>';
+            for(var key in mass) {
+                if (key == 'timeLine') {
+                    content = content + '<div class="' + mass[key] + '" ><span class="time_current"></span><span class="time_buffer"></span><span class="time_voiceover"></span><span class="time_voiceover-border"></span></div>';
+                }else if((key == 'time')){
+                    content = content + '<div class="' + mass[key] + '" ><span id="currenttime">00:00</span>/<span id="duration">00:00</span></div>';
+                }else {
+                    content = content + '<div class="' + mass[key] + '" ></div>';
+                }
             }
             content = content + '</div>';
+            var bigPlay = '<div class="'+options.bigPlay+'"></div>';
+
+            main.append(bigPlay);
             main.append(content);
+
             return main.find('.'+options.ctrlsWrap);
 
         },
-        playV:function (player, options, objects) {
-            player.play();
+        playV:function (options, objects) { //метод вопроизведения видео
+            objects.player.play();
             objects.ctrl.play.addClass('paused');
+            objects.bigPlay.addClass('paused');
             state = 1;
         },
-        pauseV:function (player, options, objects) {
-            player.pause();
+        pauseV:function (options, objects) { // метод паузы видео
+            objects.player.pause();
             objects.ctrl.play.removeClass('paused');
             state = 2;
+        },
+        togglePlay:function (options, objects) { // метод "переключатель" play/pause
+            if(state!=1){
+                methods.playV(options, objects);
+            } else{
+                methods.pauseV(options, objects);
+            }
+        },
+        stopV:function (options, objects) {// метод сброса видео в стартовое положение
+            methods.pauseV(options, objects);
+            objects.player.currentTime = 0;
+            objects.bigPlay.removeClass('paused');
+            state = 0;
+        },
+        fullscreen:function (elem) {
+
+            if (elem.requestFullscreen) {
+                elem.requestFullscreen();
+            } else if (elem.mozRequestFullScreen) {
+                elem.mozRequestFullScreen();
+            } else if (elem.webkitRequestFullscreen) {
+                elem.webkitRequestFullscreen();
+            }
+            
+        },
+        exitFullscreen:function () {
+            if(document.cancelFullScreen) {
+                document.cancelFullScreen();
+            } else if(document.mozCancelFullScreen) {
+                document.mozCancelFullScreen();
+            } else if(document.webkitCancelFullScreen) {
+                document.webkitCancelFullScreen();
+            }
+        },
+        destroy:function () {
+            $(this).html('<video src=""  poster=""></video>');
+        },
+        toggleMute : function (objects) {
+            objects.player.muted = !objects.player.muted;
+            objects.ctrl.sound.toggleClass('muted');
         }
 
     };
 
-    $.fn.trollPlayer = function(method){
+    $.fn.trollPlayer = function(method){ // конструктор
         if ( methods[method] ) {
             return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
         } else if ( typeof method === 'object' || ! method ) {
